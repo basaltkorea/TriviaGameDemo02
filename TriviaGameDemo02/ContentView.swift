14995fd91646c5613a555b9ae59b9fd25b16ed0f
basaltkorea <basalt_korea@naver.com>
2022-08-28 01:20:16 +0900

    Initial Commit

---
+++ TriviaGameDemo02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TriviaGameDemo02
+//
+//  Created by 위진수 on 2022/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriviaGameDemo02/TriviaGameDemo02App.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemo02App.swift
+//  TriviaGameDemo02
+//
+//  Created by 위진수 on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemo02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
